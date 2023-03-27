533f213e70ad1301cfd1e32d54c2d9d2977b0db7
evazaja <78253833+evazaja@users.noreply.github.com>
2023-03-27 15:29:16 +0200

    orai_megoldasok

---
+++ veletlen.js
@@ -0,0 +1,9 @@
+for (i= 1; i <=10; i++) {
+    document.write("<tr align=right>")
+    document.write("<td>"+Math.floor(Math.random()*(90-1+1))+"</td>")
+    document.write("<td>"+Math.floor(Math.random()*(90-1+1))+"</td>")
+    document.write("<td>"+Math.floor(Math.random()*(90-1+1))+"</td>")
+    document.write("<td>"+Math.floor(Math.random()*(90-1+1))+"</td>")
+    document.write("</tr>")
+    
+}
